3c3cc400f70b0baaffd8864aedb61a288268974d
Inteyaz Alam <inteyazalam432@gmail.com>
2026-04-11 14:52:45 +0530

    Backend developent
    IN this I make a server for backend and make a routes and controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,17 @@
+// route for user login
+const loginUser = async (req,res) => {
+
+}
+
+// Route for user register
+
+const registerUser = async (req,res) =>{
+      res.json({message:" Register API Working"})
+}
+
+// Route for admin login
+const adminLogin = async(req,res)=>{
+
+}
+
+export {loginUser,registerUser,adminLogin} 
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { connect } from 'mongoose'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoutes.js'
+
+//App Config
+
+const app = express()
+const port = process.env.PORT || 4000 
+connectDB()
+connectCloudinary()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+//Api endpoints
+app.use('/api/user',userRouter)
+// app.user('/api/')
+app.get('/',(req,res)=>{
+    res.send("API working")
+})
+
+app.listen(port,()=>console.log('Server started on PORT :' + port))
