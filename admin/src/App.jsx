6ae9ee3ed51920567141f2614ed94f3e676130b4
Inteyaz Alam <inteyazalam432@gmail.com>
2026-04-18 11:21:16 +0530

    Admin panel add product page
    In this i make a formdata page which stores the value of input and connect with the backend so that it does not make any error product adding functonality is working

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,12 +9,16 @@ import Orders from './Pages/Orders'
 import Login from './Components/Login'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react'
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
  
 const App = () => {
 
-  const [token,setToken] = useState("")
+  const [token,setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):"")
+  useEffect(()=>{
+    localStorage.setItem('token',token)
+  },[token])
 
   return (
     <div className='bg-gray-50 min-h-screen'>
@@ -22,18 +26,18 @@ const App = () => {
       { token === ""
       ? <Login  setToken = {setToken}/>
       :   <>
-           <Navbar/>
+           <Navbar setToken ={setToken}/>
            <hr />
            <div className='flex w-full'> 
             <Sidebar />
             <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
               <Routes>
-                <Route path="/" element={<Add />} />   
+                {/* <Route path="/" element={<Add />} />    */}
                 
-                <Route path="/add" element={<Add />}/>
-                <Route path="/list" element={<List />}/>
-                <Route path="/orders" element={<Orders />}/>
-                 <Route path="*" element={<Add />} /> 
+                <Route path="/add" element={<Add token = {token}/>}/>
+                <Route path="/list" element={<List token = {token}/>}/>
+                <Route path="/orders" element={<Orders token = {token}/>}/>
+                 {/* <Route path="*" element={<Add />} />  */}
               </Routes>
              
             </div>
